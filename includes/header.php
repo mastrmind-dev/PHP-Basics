ded4611c88131a17bb433a05970f127826097acc
mastrmind-dev <asapthaka@gmail.com>
2021-03-17 12:22:52 +0530

    final commit

---
+++ includes/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>PHP Primer - <?php echo $title; ?></title>
+</head>
+
+<body>
+    <h2>Follow each link to the page example</h2>
+    <ul>
+        <li><a href="array.php">Simple array and printouts</a></li>
+        <li><a href="forloop.php">Simple for loop</a></li>
+        <li><a href="dowhileloop.php">Simple do while</a></li>
+        <li><a href="whileloop.php">Simple while</a></li>
+        <li><a href="ifstatement.php">Simple if statement</a></li>
+        <li><a href="switchstatement.php">Simple switch statement</a></li>
+        <li><a href="stringmanip.php">Simple string manipulation</a></li>
+    </ul>
+    <h1>
+        Hello World - PHP Primer
+    </h1>
---
+++ stringmanip.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Primer - String manipulation</title>
+</head>
+<body>
+    <?php
+    $phrase1 = 'My name ';
+    $phrase2 = 'is Sapthaka.';
+    $phrase3 = 'something is best.';
+    
+    function test(){
+
+        return 'fu';
+    }
+    
+    echo test();
+    
+    ?>
+</body>
+</html>
